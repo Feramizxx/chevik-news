761a849b8d77dff07bd19f8bc742e3ca3d05dc53
mtagizadee <magatagi0@gmail.com>
2022-09-04 01:38:08 +0400

    taiwind was initialized

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='flex items-center justify-center w-screen h-screen'>
             Home Page
         </div>
     );
