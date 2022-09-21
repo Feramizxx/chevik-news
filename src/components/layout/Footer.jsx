06d72f570043d30f8eda0469e844ea4b72157cad
Feramiz Bahaddinov <feramizbahadinnov@gmail.com>
2022-09-21 19:50:58 +0400

    fixed some problems

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,22 +7,21 @@ import Logo from "../../assets/img/logo.png";
 
 const Footer = () => {
   return (
-   
-      <div className="mt-24 ">
-        <hr className="bg-line h-[1px] border-none" />
-        <div className="flex items-center justify-center py-16 px-6">
-          <p className="max-w-[600px]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged
-          </p>
-        </div>
-        {/* p-6 sm:p-12 lg:p-8   sm:grid grid-cols-2 lg:flex justify-evenly */}
-        <footer className="bg-primary-card flex flex-col">
-          <div className="grid grid-cols-4 max-lg:grid-cols-2 max-lg:place-items-start max-sm:grid-cols-1 place-items- px-16 py-8 ">
+    <div className="mt-24 ">
+      <hr className="bg-line h-[1px] border-none" />
+      <div className="flex items-center justify-center py-16 px-6">
+        <p className="max-w-[600px]">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged
+        </p>
+      </div>
+      {/* p-6 sm:p-12 lg:p-8   sm:grid grid-cols-2 lg:flex justify-evenly */}
+      <footer className="bg-primary-card flex flex-col">
+        <div className="grid grid-cols-4 max-lg:grid-cols-2 max-lg:place-items-start max-sm:grid-cols-1 place-items- px-16 py-8 ">
           <div>
             <img className="mb-2" src={Logo} />
             <p className="max-w-[450px]">
@@ -31,7 +30,6 @@ const Footer = () => {
               in tourism industry from different sector of travel industry.
               Highest service quality within stricted time is our main aim.
               Guests happiness = our team’s happiness.
-             
             </p>
           </div>
           <div className="pl-20 max-lg:pl-32 max-sm:pl-0">
@@ -74,8 +72,11 @@ const Footer = () => {
                   href="https://www.google.com/maps/place/7+Heyd%C9%99r+%C6%8Fliyev+prospekti,+Bak%C4%B1+1033,+%D0%90%D0%B7%D0%B5%D1%80%D0%B1%D0%B0%D0%B9%D0%B4%D0%B6%D0%B0%D0%BD/@40.3976489,49.8697907,17z/data=!3m1!4b1!4m5!3m4!1s0x40307d4789526da9:0xd89917404bcaeea1!8m2!3d40.3976448!4d49.8719794"
                   target="_blank"
                 >
-                  7 H.Aliyev, Baku, Azerbaijan AZ1010
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla ut possimus praesentium sint saepe. Nihil distinctio error, aliquid qui nobis itaque sapiente ea quia praesentium dolorem ut cupiditate consequatur suscipit.
+                  7 H.Aliyev, Baku, Azerbaijan AZ1010 Lorem ipsum, dolor sit
+                  amet consectetur adipisicing elit. Nulla ut possimus
+                  praesentium sint saepe. Nihil distinctio error, aliquid qui
+                  nobis itaque sapiente ea quia praesentium dolorem ut
+                  cupiditate consequatur suscipit.
                 </a>
               </li>
               <li className="my-1 no-underline">
@@ -91,23 +92,27 @@ const Footer = () => {
             </ul>
           </div>
           {/* © Bütün hüquqlar qorunur <TheJavaChip className="ml-6" />*/}
-          <div className="max-lg:pl-32 max-sm:pl-0" >
+          <div className="max-lg:pl-32 max-sm:pl-0">
             <h3 className="no-underline"> Sosial şəbəkələrimiz </h3>
             <div className="flex justify-between max-w-[170px]">
               <Instagram />
               <Facebook />
               <Telegram />
-       
             </div>
           </div>
-       </div>
-       <div className="px-16 mt-4 flex w-full justify-between flex-row  max-sm:flex-col"> <p> © Bütün hüquqlar qorunur </p>
-         <p className="flex  items-center max-sm:pt-4"> Site created by: <TheJavaChip className="ml-6" /></p></div>
-        </footer>
-      
-      </div>
-
-    
+        </div>
+        <div className="px-16 mt-4 flex w-full justify-between flex-row  max-sm:flex-col">
+          
+          <p> © Bütün hüquqlar qorunur </p>
+          <div className="flex items-center">
+            <p className="flex   items-center max-sm:pt-4"> Site created by: </p>
+            <span className="w-[180px] ml-2 mb-2 bg-white">
+              <TheJavaChip className="ml-6" />
+            </span>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
